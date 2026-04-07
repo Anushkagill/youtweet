50de773d39ad8ea840decef928b5adca9daf06c7
Anushkagill <anushkagill01@gmail.com>
2026-04-07 22:32:29 +0530

    enhance like model documentation with detailed comments on pre-save middleware and index constraints

--- src/models/like.model.js
+++ src/models/like.model.js
@@ -25,7 +25,9 @@ const likeSchema=new Schema(
         timestamps:true
     }
 )
-
+/* so here we are defining the pre-save middleware for the likeSchema because we want to check before 
+saving that this function is called which ensures only one of the three fields is present because of
+one like only one count will increase in any of the one video,comment,tweet */
 likeSchema.pre("save", function (next) {
     const count =
         (this.video ? 1 : 0) +
@@ -39,6 +41,9 @@ likeSchema.pre("save", function (next) {
     next();
 });
 
+/*Agar aisa document bana jisme likedBy aur video field exactly same hai to an already created document, toh ye document create nhi hoga
+aur jahan Like.create() call hua hai vahan ek error throw hoga "MongoServerError: E11000 duplicate key error collection: subscriptions"
+jiska error code 11000 hoga*/
 
 likeSchema.index(
     { likedBy: 1, video: 1 },
@@ -54,5 +59,6 @@ likeSchema.index(
     { likedBy: 1, tweet: 1 },
     { unique: true, partialFilterExpression: { tweet: { $exists: true } } }
 );
+//ye indexes ensure karte hai ki ek user ek hi video, comment, ya tweet ko like kar sakta hai, aur agar wo try karta hai toh usko duplicate key error milega
 
 export const Like=mongoose.model("Like",likeSchema) 
