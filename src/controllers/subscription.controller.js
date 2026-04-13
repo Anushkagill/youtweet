fa6959676175191ac1ffbf3b6e9917a59fd58d21
Anushkagill <anushkagill01@gmail.com>
2026-04-14 01:03:45 +0530

    feat: implement pagination and user details retrieval for channel subscribers

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -53,9 +53,68 @@ const toggleSubscription = asyncHandler(async (req, res) => {
         throw new ApiErrors(500, "Internal server error while subscribing")
     }
 })
+
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
-    const {channelId} = req.params
+    const { channelId } = req.params
+    const { page = 1, limit = 10 } = req.query
+
+    if (!channelId || !mongoose.Types.ObjectId.isValid(channelId)) {
+        throw new ApiErrors(400, "Invalid channel id")
+    }
+
+    const pipeline = [
+        {
+            $match: {
+                channel: new mongoose.Types.ObjectId(channelId)
+            }
+        },
+        {
+            $sort: {
+                createdAt: -1//latest subscriber will be on top
+            }
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "subscriber",
+                foreignField: "_id",
+                as: "subscriber",
+                pipeline: [
+                    {
+                        $project: {
+                            fullName: 1,
+                            username: 1,
+                            avatar: 1
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $unwind: "$subscriber"
+        },
+        {
+            $project: {
+                subscriber: 1
+            }
+        }
+    ]
+
+    const options = {
+        page: Number(page),
+        limit: Number(limit)
+    }
+
+    
+    const subscribers = await Subscription.aggregatePaginate(
+        Subscription.aggregate(pipeline),
+        options
+    )
+
+    return res.status(200).json(
+        new ApiResponse(200, subscribers, "Subscribers fetched successfully")
+    )
 })
 
 // controller to return channel list to which user has subscribed
